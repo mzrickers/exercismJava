b26be162643898c253f2111deb218483f75bfb5a
Zach Rickers <mzrickers@gmail.com>
2020-11-12 17:55:41 -0700

    Two fer second ternery

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,23 @@
+import java.util.Integer;
+
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String result = "";
+        if (number % 3 == 0) {
+            result += "Pling";
+        }
+        if (number % 5 == 0) {
+            result += "Plang";
+        }
+        if (number % 7 == 0) {
+            result += "Plong";
+        }
+        if (result == "") {
+            return result;
+        } else {
+            return Integer.toString(number);
+        }
     }
-
 }
+
--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,9 +1,7 @@
+
+
 public class Twofer {
     public String twofer(String name) {
-        if (name == null) {
-            return "One for you, one for me.";
-        } else {
-            return ("One for " + name + ", one for me.");
-        }
+        return name == null ? "One for you, one for me." : "One for " + name + ", one for me.";
     }
 }
